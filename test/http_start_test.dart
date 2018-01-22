ae41b875eb8c3c2c21a7f9890324b699cc58cbb5
Furov3 <soultaper@gmail.com>
2018-01-22 19:47:13 +0300

    test template

---
+++ test/http_start_test.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+import 'dart:async';
+import '../bin/http_srv.dart';
+
+
+void main() {
+  test('Server sucsess start',() {
+    expect(... , prints('Listen adress on 8080'));
+  });
+  test('HTTP response',() {
+    expect(... ,);
+  });
+}
