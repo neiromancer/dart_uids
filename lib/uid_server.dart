8eee9097f19f1ea371e6e232fc7da565af98ba2e
Furov3 <soultaper@gmail.com>
2018-01-30 20:05:12 +0300

    Пулл реквест3

--- lib/uid_server.dart
+++ lib/uid_server.dart
@@ -4,25 +4,28 @@ import 'package:dart_uids/src/uid_generator.dart';
 
 /// HTTP_UID сервер.
 class UidServer {
-  // конструктор HTTP сервера.
+  /// переменная сервер.
+  HttpServer _server;
+  /// порт HTTP сервера.
   final int _port;
+  /// адрес HTTP сервера.
   final String _host;
+  /// создает UID сервер.
   UidServer(String host, int port)
       : _host = host,
         _port = port;
-  /// объявляет переменную и присваеивает ей экземпляр сервера
-  HttpServer _server;
+
 
   /// Запускает сервер и слушает запросы.
   Future start() async {
-    /// Инициализация сервера.
+
+    // Инициализация сервера.
     _server = await HttpServer.bind(
       _host,
       _port,
     );
 
     // Печатает в консоль отчет о успешном запуске и актуальном адрес:порт.
-    // считанном из запущенного сервера.
     print('Listening on ${_server.address}:${_server.port}');
 
     // Обрабатывает запрос к серверу.
@@ -36,18 +39,18 @@ class UidServer {
   }
 }
 
-/// Функция обработки запроса и ответа на него.
-void _handleRequest(request) {
-  //преверка запроса на валидность.
-  if (request.method == 'GET' && request.uri.path == "/") {
-    // если запрос валидный то получение UID и ответ клиенту.
-    String uid = new UidGenerator().generate();
-    request.response.write(uid);
-  } else {
-    // если запрос плохой ответ bad request.
-    request.response.statusCode = HttpStatus.BAD_REQUEST;
-  }
+/// Обработки запроса и ответа на него.
+  void _handleRequest(request) {
+    // преверка запроса на валидность.
+    if (request.method == 'GET' && request.uri.path == "/") {
+      // если запрос валидный то получение UID и ответ клиенту.
+      String uid = new UidGenerator().generate();
+      request.response.write(uid);
+    } else {
+      // если запрос плохой ответ bad request.
+      request.response.statusCode = HttpStatus.BAD_REQUEST;
+    }
 
-  // Закрывает ответ.
-  request.response.close();
-}
+    // Закрывает ответ.
+    request.response.close();
+  }
